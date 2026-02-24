6f899a70a2ac9f95d0e5da14da1172e9e4c87bc7
Jefferson Dela Cruz <163842744+Jeeruz@users.noreply.github.com>
2026-02-24 14:54:18 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,14 +5,12 @@ const ASSETS = [
   '/pwa-test/manifest.json'
 ];
 
-// Cache the necessary files
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Offline support: Network first, fallback to cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
